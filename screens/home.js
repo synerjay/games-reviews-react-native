3288ac22a542abb4c1f86d38197f223ee43425e4
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-01 19:22:04 +0900

    added navigation props

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate('Review Details');
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title='to review details screen' onPress={pressHandler} />
     </View>
   );
 }
