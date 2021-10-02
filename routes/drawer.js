1679ad9db04f29bdda0cf721012d3629bbfd4a2c
Jerome Tolentino <tolent.jay@gmail.com>
2021-10-02 14:31:51 +0900

    Solved the drawer problem

--- routes/drawer.js
+++ routes/drawer.js
@@ -10,8 +10,16 @@ export default function DrawerNavigator() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='Home'>
-        <Drawer.Screen name='Home' component={HomeStack} />
-        <Drawer.Screen name='About' component={AboutStack} />
+        <Drawer.Screen
+          name='Home'
+          component={HomeStack}
+          options={{ headerShown: false }} // this must be set to false to not have the drawer header and stack header on top of each other!!
+        />
+        <Drawer.Screen
+          name='About'
+          component={AboutStack}
+          options={{ headerShown: false }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
